ad4559699631b15c4169fedbb53ad95f70e92c2d
aliabbasi2024 <aliabbassi68@gmail.com>
2024-04-04 23:28:27 +0430

    add file

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php 
+$names = [
+    [
+        "name" => "saman",
+        "age" => 29,
+    ],
+    [
+        "name" => "ali",
+        "age" => 35,
+    ],
+    [
+        "name" => "kambiz",
+        "age" => 10,
+    ],
+    [
+        "name" => "seti",
+        "age" => 52,
+    ],
+    [
+        "name" => "eli",
+        "age" => 36,
+    ],
+    [
+        "name" => "teymoor",
+        "age" => 12,
+    ]
+ ];
+
+ foreach($names as $name){
+    if($name['age'] >= 18){
+        echo $name['name'] ;
+        echo "<br><br>";    
+    }
+       
+ }
+ // git init git add .
+ 
+ /*
+ 1-git end 
+ 2-form end
+ 3- file upload end 
+ 4- 
+
+ /JQuery/
+ 
+ */
