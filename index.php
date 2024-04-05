8300da5a4cc2bb01374680fb4281a3d2dd42dbc7
aliabbasi2024 <aliabbassi68@gmail.com>
2024-04-05 15:18:06 +0430

    add file

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php 
-$names = [
-    [
-        "name" => "saman",
-        "age" => 29,
-    ],
-    [
-        "name" => "ali",
-        "age" => 35,
-    ],
-    [
-        "name" => "kambiz",
-        "age" => 10,
-    ],
-    [
-        "name" => "seti",
-        "age" => 52,
-    ],
-    [
-        "name" => "eli",
-        "age" => 36,
-    ],
-    [
-        "name" => "teymoor",
-        "age" => 12,
-    ]
- ];
+// $names = [
+//     [
+//         "name" => "saman",
+//         "age" => 29,
+//     ],
+//     [
+//         "name" => "ali",
+//         "age" => 35,
+//     ],
+//     [
+//         "name" => "kambiz",
+//         "age" => 10,
+//     ],
+//     [
+//         "name" => "seti",
+//         "age" => 52,
+//     ],
+//     [
+//         "name" => "eli",
+//         "age" => 36,
+//     ],
+//     [
+//         "name" => "teymoor",
+//         "age" => 12,
+//     ]
+//  ];
 
- foreach($names as $name){
-    if($name['age'] >= 18){
-        echo $name['name'] ;
-        echo "<br><br>";    
-    }
+//  foreach($names as $name){
+//     if($name['age'] >= 18){
+//         echo $name['name'] ;
+//         echo "<br><br>";    
+//     }
        
- }
+//  }
  // git init git add .
  
  /*
@@ -44,3 +44,13 @@ $names = [
  /JQuery/
  
  */
+?>
+ <form action="addnew.php" method="get">
+    <!-- <select name="list" id="">
+        <option value="ali">ali</option>
+        <option value="hadi">hadi</option>
+        <option value="saman">saman</option>
+    </select> -->
+    <input type="checkbox" name="" id="">
+    <button type="submit">Submit</button>
+ </form>
